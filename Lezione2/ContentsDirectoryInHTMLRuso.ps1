165a5862edb12eb027d50117b1ca2c1bb1a29fc4
RusoSfuso <61048779+Rusothereal@users.noreply.github.com>
2023-03-10 17:17:12 +0100

    Create ContentsDirectoryInHTMLRuso.ps1

---
+++ Lezione2/ContentsDirectoryInHTMLRuso.ps1
@@ -0,0 +1,19 @@
+﻿<# -----------------------------------------------------------------------------
+
+                                SCRIPT IN WINDOWS
+
+    FUNCTION : List contents from C:\Winwdows folder & convert data into an HTML page
+
+        WRITER : Emanuele Caruso
+
+            PUBLISHED IN : 10/03/2023
+
+                LAST UPDATE : 10/03/2023
+
+
+!! USE THE CODE AT YOUR RISK, ALL RIGHTS ARE AT THE PRODUCER, NOT SHARE OR RUSO WILL FIND AND PUNISH YOU !!
+
+----------------------------------------------------------------------------- #>
+
+$Directory = "C:\Windows"
+Get-ChildItem $Directory | Select-Object Name |ConvertTo-Html | Out-File "C:\Users\user\Desktop\WindowsDIR.html"
