35612171c2ee84925b61cacfec46656c70b2d4dd
RusoSfuso <61048779+Rusothereal@users.noreply.github.com>
2023-03-10 15:46:23 +0100

    Create RestartServiceByRuso.ps1

---
+++ Lezione2/RestartServiceByRuso.ps1
@@ -0,0 +1,23 @@
+﻿<# -----------------------------------------------------------------------------
+
+                                SCRIPT IN WINDOWS
+
+    FUNCTION : Restart Spooler Service
+
+        WRITER : Emanuele Caruso
+
+            PUBLISHED IN : 10/03/2023
+
+                LAST UPDATE : 10/03/2023
+
+
+!! USE THE CODE AT YOUR RISK, ALL RIGHTS ARE AT THE PRODUCER, NOT SHARE OR RUSO WILL FIND AND PUNISH YOU !!
+
+----------------------------------------------------------------------------- #>
+
+<# 1st Method #>
+$Servizio = Spooler
+Restart-Service $Servizio
+
+<# 2nd Method #>
+Get-Service -Name $Servizio | Restart-Service
