380bfe1bcb605d9c4424db71e0d2965b07f1fd5d
RusoSfuso <61048779+Rusothereal@users.noreply.github.com>
2023-03-10 17:32:45 +0100

    Slide

---
+++ Lezione2/OutputStatoServiziRuso.ps1
@@ -0,0 +1,32 @@
+﻿<# -----------------------------------------------------------------------------
+
+                                SCRIPT IN WINDOWS
+
+    FUNCTION : 
+
+        WRITER : Emanuele Caruso
+
+            PUBLISHED IN : 10/03/2023
+
+                LAST UPDATE : 10/03/2023
+
+
+!! USE THE CODE AT YOUR RISK, ALL RIGHTS ARE AT THE PRODUCER, NOT SHARE OR RUSO WILL FIND AND PUNISH YOU !!
+
+----------------------------------------------------------------------------- #>
+
+<# Output formato tabella ordinato per stato #> 
+
+Get-Service | Sort-Object -Property STATUS
+
+
+<# uguale al precedente ma specificando le proprietà che vogliamo utilizzare #>
+
+Get-Service | Sort-Object STATUS | Format-Table -Property NAME, STATUS -AutoSize
+
+
+<# Filtra quelli in stato running #>
+
+Get-Service | Where-Object { $_.Status -eq 'Running' }
+Get-Service | Where-Object Status -eq 'Running' }
+
---
+++ Lezione2/ProprietàProcessiRuso.ps1
@@ -0,0 +1,29 @@
+﻿<# -----------------------------------------------------------------------------
+
+                                SCRIPT IN WINDOWS
+
+    FUNCTION :
+
+        WRITER : Emanuele Caruso
+
+            PUBLISHED IN : 10/03/2023
+
+                LAST UPDATE : 10/03/2023
+
+
+!! USE THE CODE AT YOUR RISK, ALL RIGHTS ARE AT THE PRODUCER, NOT SHARE OR RUSO WILL FIND AND PUNISH YOU !!
+
+----------------------------------------------------------------------------- #>
+<# Ottenere informazioni sui processi in esecuzione #> 
+
+Get-Process | Select-Object -Property Name
+
+
+<# Ottenerle in formato lista #>
+
+Get-Process | Format-List -Property Name, ID
+
+
+<# Ottenerle in formato tabella #>
+
+Get-Process | Format-Table -Property Name, ID
