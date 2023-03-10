42103b65030cdb0def187b73e3658392316d8955
RusoSfuso <61048779+Rusothereal@users.noreply.github.com>
2023-03-10 15:15:01 +0100

    Create esManipulazioneRusoeTeo.ps1

---
+++ Lezione2/esManipulazioneRusoeTeo.ps1
@@ -0,0 +1,15 @@
+﻿<# Selezionare una directory di un file #>
+
+$Directory = "C:\Windows\System32\drivers\etc\lmhosts.sam"
+
+
+<# Trovare la lunghezza della directory per individuare poi l'ultimo file qui presente #>
+
+$Grandezza = $Directory.Split("\").Count
+$Grandezza = $Grandezza -1
+
+
+<# Visualizzare l'ultimo file presente nella directory #>
+
+$Directory = $Directory.Split("\")[$Grandezza]
+Write-Host $Directory
