99f74ca51aef432d24ddb0425941775ce839437d
Reda2200 <119880230+Reda2200@users.noreply.github.com>
2023-03-10 15:29:17 +0100

    Create esmanipolazioneTeo.ps1

---
+++ Lezione2/esmanipolazioneTeo.ps1
@@ -0,0 +1,7 @@
+﻿#Matteo Redaelli
+
+$path = "C:\Windows\System32\drivers\etc\lmhosts.sam\pippo"
+$var = $path.Split("\").Count - 1
+$var2 = $var - 1
+$final = $path.Split("\")[$var2]
+Write-Host $final
