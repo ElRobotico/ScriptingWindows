afd4a43284b602e151e35b75ddc0e26756c4a252
RusoSfuso <61048779+Rusothereal@users.noreply.github.com>
2023-03-10 14:47:34 +0100

    Create Servizi.ps1

---
+++ Lezione1/Servizi.ps1
@@ -0,0 +1,12 @@
+﻿<# Recuperare l'help del comando Get-Service #>
+Get-Help Get-Service
+
+<# Aggiornare le guide dei comandi #>
+Update-Help
+
+<# Visualizzare Alias #>
+Get-Alias -Definition Get-Service
+
+<# Ottenere servizio "Print Spooler" e riavviarlo #>
+Get-Service -Name "Spooler"
+Restart-Service -Name "Spooler"
