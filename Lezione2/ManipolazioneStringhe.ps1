c746f1fcd31a8a80d3f2a16179516fe437aa608c
Flaam <48182219+Flaam@users.noreply.github.com>
2023-03-10 14:44:02 +0100

    Array

--- Lezione2/ManipolazioneStringhe.ps1
+++ Lezione2/ManipolazioneStringhe.ps1
@@ -3,3 +3,6 @@ $String = "Hello World!" # Variabile
 $String.Length # Lunghezza in caratteri
 $String.ToUpper() # Uppercase
 $String.ToLower() # Lowercase
+
+$Array = "One","Two","Three"
+$Array -Join " | "
