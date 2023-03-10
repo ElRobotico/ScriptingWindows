352272a2292ac8dd625993e60c1d81fa0de8fcb1
Flaam <48182219+Flaam@users.noreply.github.com>
2023-03-10 15:07:09 +0100

    Create EsercizioManipolazioneStringhe.ps1

---
+++ Lezione2/EsercizioManipolazioneStringhe.ps1
@@ -0,0 +1,4 @@
+# Creare uno script che, dato in ingresso un percorso completo di un file, ritorni in uscita solo il nome del file.
+
+$Directory = "C:\Windows\System32\drivers\etc\lmhosts.sam" # Directory
+$Directory.Split('\')[5] # Split dell'array con '\' e scelta della 5a row
