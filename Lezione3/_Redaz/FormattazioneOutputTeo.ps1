d7759544daedad703dc684346006505c444e848a
Reda2200 <119880230+Reda2200@users.noreply.github.com>
2023-03-17 14:53:07 +0100

    Create FormattazioneOutputTeo.ps1

---
+++ Lezione3/_Redaz/FormattazioneOutputTeo.ps1
@@ -0,0 +1,27 @@
+﻿# FormattazioneOutputTeo.ps1
+#
+# Version History
+# v0.1 -STESURA INIZIALE - Genera una lista dei primi 20 processi in esecuzione, in ordine decrescente in base all'ID.
+#                          In fine esporta la lista in un file CSV nella cartella C:\Temp
+#
+# Autore: Matteo Redaelli - matteo.redaelli@itstechtalentfactory.it
+# Data prima stesura: 10/03/2023
+# Data ultima modifica: 17/03/2023
+# Ultimo autore: Matteo Redaelli - matteo.redaelli@itstechtalentfactory.it
+#
+# Qualsiasi riproduzione non autorizzata, anche parziale, e'vietata
+#
+# NOTE IMPORTANTI:
+# -Non effettuate alcuna modifica se non sapete dove mettere le mani
+# REQUISITI:
+# Eseguire lo script solo da Amministratore
+# Avere una cartella C:\Temp
+
+$DatadiOggi = Get-Date -Format "ddMMyyyy"
+# crea una variabile nel quale salvare la data di oggi nel formato giorno/mese/anno
+
+$path = "C:\Temp"
+# crea una variabile nel quale l'utente può indicare la cartella a piacere nel quale salvare il file
+
+Get-Process | Select-Object -First 20 | Sort-Object ID -Descending | Export-Csv -Path "$path\$DatadiOggi`_Process.csv"
+# tramite get-process si estraggono tutti processi in esecuzione, Select-object seleziona i primi 20 processi, sort-object li ordina in base all'ID in ordine decrescente, export-csv esporta il file nella cartella scelta a piacere 
