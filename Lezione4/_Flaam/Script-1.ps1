e0df259cbdba3e0d8d68e128abd2e542788635ea
Flaam <48182219+Flaam@users.noreply.github.com>
2023-03-24 15:11:53 +0100

    ssss

---
+++ Lezione4/_Flaam/Script-1.ps1
@@ -0,0 +1,16 @@
+﻿################################################
+# Script-1.ps1
+################################################
+# Version History
+# v0.1 | Prima versione
+################################################
+# Autore: Marco - flaam@nebulacorp.xyz
+# Data prima stesura: 24/03/2023
+# Data ultima modifica: 24/03/2023
+# Ultimo autore: Marco - flaam@nebulacorp.xyz
+################################################
+
+$path = "C:\Windows"
+Get-ChildItem $path -File | ForEach-Object {
+    Write-Host $_.Name $_.Name.Length
+}
---
+++ Lezione4/_Flaam/Script-2.ps1
@@ -0,0 +1,16 @@
+﻿################################################
+# Script-2.ps1
+################################################
+# Version History
+# v0.1 | Prima versione
+################################################
+# Autore: Marco - flaam@nebulacorp.xyz
+# Data prima stesura: 24/03/2023
+# Data ultima modifica: 24/03/2023
+# Ultimo autore: Marco - flaam@nebulacorp.xyz
+################################################
+
+foreach ($service in Get-Service) {
+    if ($service.Status -eq "Running"){ Write-Host $service.name $service.status -ForegroundColor Green}
+    if ($service.Status -eq "Stopped"){ Write-Host $service.name $service.status -ForegroundColor Red}
+}
