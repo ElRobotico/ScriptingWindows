757f7a380895d735266e81e4e0ea3ee2b3eb1538
RusoSfuso <61048779+Rusothereal@users.noreply.github.com>
2023-03-24 15:19:49 +0100

    Create EsercizioGPT.ps1

---
+++ Lezione4/_Ruso/EsercizioGPT.ps1
@@ -0,0 +1,38 @@
+﻿<# -----------------------------------------------------------------------------
+
+                                SCRIPT IN WINDOWS
+
+    FUNCTION :
+
+        WRITER : Emanuele Caruso
+
+            PUBLISHED IN : 10/03/2023
+
+                LAST UPDATE : 10/03/2023
+
+
+!! USE THE CODE AT YOUR RISK, ALL RIGHTS ARE AT THE PRODUCER, NOT SHARE OR RUSO WILL FIND AND PUNISH YOU !!
+
+----------------------------------------------------------------------------- #>
+
+
+# Elencare il contenuto della cartella c:windows\temp e stampare quanti caratteri ha ogni file
+
+Get-ChildItem C:\Windows\Temp | ForEach-Object { 
+    Write-Host $_.Name.Length $_.Name 
+}
+
+
+
+
+# Elencare i servizi e colorarli in rosso se posti in stato stopped oppure in verde se in running
+
+Get-Service | ForEach-Object {
+    $serviceName = $_.Name
+    $serviceStatus = $_.Status
+    if ($serviceStatus -eq "Running") {
+        Write-Host $serviceName -ForegroundColor Green
+    } else {
+        Write-Host $serviceName -ForegroundColor Red
+    }
+}
