e08d47b6574a935f13369576433a16a8a6f7b342
Reda2200 <119880230+Reda2200@users.noreply.github.com>
2023-03-24 12:10:51 +0100

    es

---
+++ Lezione4/_Redaz/Script-1(23-03-2023).ps1
@@ -0,0 +1,21 @@
+﻿# Script-1.ps1
+#
+# Version History
+# v0.1 -STESURA INIZIALE - stampa a video il nome del file e il numero di caratteri da cui è composto
+#
+# Autore: Matteo Redaelli - matteo.redaelli@itstechtalentfactory.it
+# Data prima stesura: 24/03/2023
+# Data ultima modifica: 24/03/2023
+# Ultimo autore: Matteo Redaelli - matteo.redaelli@itstechtalentfactory.it
+#
+# Qualsiasi riproduzione non autorizzata, anche se parziale, e'vietata
+#
+# NOTE IMPORTANTI:
+# -Non effettuate alcuna modifica se non sapete dove mettere le mani
+# REQUISITI:
+# Eseguire lo script solo da Amministratore
+# Avere una cartella C:\Windows
+$path = "C:\Windows"
+Get-ChildItem $path -File | ForEach-Object {
+    Write-Host $_.Name $_.Name.Length
+}
---
+++ Lezione4/_Redaz/Script-2(23-03-2023).ps1
@@ -0,0 +1,21 @@
+﻿# Script-2.ps1
+#
+# Version History
+# v0.1 -STESURA INIZIALE - Mostra i tutti i servizi presenti sul  computer in verde se si trovano in esecuzione, in rosso se fermi
+#
+# Autore: Matteo Redaelli - matteo.redaelli@itstechtalentfactory.it
+# Data prima stesura: 24/03/2023
+# Data ultima modifica: 24/03/2023
+# Ultimo autore: Matteo Redaelli - matteo.redaelli@itstechtalentfactory.it
+#
+# Qualsiasi riproduzione non autorizzata, anche se parziale, e'vietata
+#
+# NOTE IMPORTANTI:
+# -Non effettuate alcuna modifica se non sapete dove mettere le mani
+# REQUISITI:
+# Eseguire lo script solo da Amministratore
+
+foreach ($service in Get-Service) {
+    if ($service.Status -eq "Running"){ Write-Host $service.name $service.status -ForegroundColor Green}
+    if ($service.Status -eq "Stopped"){ Write-Host $service.name $service.status -ForegroundColor Red}
+}
