49d1907a634f829fc8384d93f08dd49ada17954b
Flaam <48182219+Flaam@users.noreply.github.com>
2023-03-31 09:30:52 +0200

    Lez5

---
+++ Lezione5/_Flaam/Script.ps1
@@ -0,0 +1,11 @@
+################################################
+# Script1.ps1
+################################################
+# Version History
+# v0.1 | Prima versione
+################################################
+# Autore: Marco - flaam@nebulacorp.xyz
+# Data prima stesura: 31/03/2023
+# Data ultima modifica: 31/03/2023
+# Ultimo autore: Marco - flaam@nebulacorp.xyz
+################################################
