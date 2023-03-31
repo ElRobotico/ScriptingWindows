a3b159caa1bd0d6213080728a7d978e0740c0e28
Reda2200 <119880230+Reda2200@users.noreply.github.com>
2023-03-31 19:34:38 +0200

    v

---
+++ Lezione5/_Redaz/es log colorati.ps1
@@ -0,0 +1,18 @@
+﻿$data = Get-Date -format "HH:mm dd/MM/yyyy"
+$color = Read-Host 
+
+function log {
+    param ($messaggio, $colore)
+    if ($colore -eq "Green") {
+    $messaggio = "Info | $messaggio"
+    Write-Host "$messaggio " -ForegroundColor $colore}
+    if ($colore -eq "Yellow") {
+    $messaggio = "Warning | $messaggio"
+    Write-Host "$messaggio " -ForegroundColor $colore}
+    if ($colore -eq "Red") {
+    $messaggio = "ERROR | $messaggio"
+    Write-Host "$messaggio " -ForegroundColor $colore}
+    Return $messaggio
+}
+
+$output = log -messaggio "Inizio Esecuzione alle $data" -colore "$color" | Out-File -FilePath "C:\Temp\log.txt" -Append
---
+++ Lezione5/_Redaz/eslogprocessi.ps1
@@ -0,0 +1,33 @@
+﻿$servizi = Get-Service | Where-Object status -eq "Running" | Sort-Object Name
+$dataora = Get-Date -Format dd-MM-yyyy_HH-mm-ss
+$path = "C:\Script\Archivio\servizi_attivi" + "_$dataora" + ".csv"
+
+if (Test-Path "C:\Script\servizi_attivi.csv"){
+
+    if (Test-Path "C:\Script\Archivio") {
+
+    Move-Item -Path "C:\Script\servizi_attivi.csv" -Destination $path
+    $servizi | Export-Csv "C:\Script\servizi_attivi.csv"
+
+    }else{
+
+    New-Item -Path "C:\Script\Archivio" -ItemType Directory
+    Move-Item -Path "C:\Script\servizi_attivi.csv" -Destination $path
+    $servizi | Export-Csv "C:\Script\servizi_attivi.csv"
+
+    }
+
+} else {
+    $servizi | Export-Csv "C:\Script\servizi_attivi.csv"
+}
+
+$pathlog = "C:\Temp\ScheduledTaskRun.log"
+$datalog = Get-Date -Format dd-MM-yyyy
+$oralog = Get-Date -Format HH:mm:ss
+$numeroserviziR = (Get-Service | Where-Object status -eq "Running").count
+$numeroserviziS = (Get-Service | Where-Object status -eq "Stopped").count
+
+Add-Content -path $pathlog "-----------------------------------------------------------"
+Add-Content -path $pathlog "# Esecuzione dello script in data: $datalog ore $oralog" 
+Add-Content -path $pathlog "#   Servizi attivi: $numeroserviziR"
+Add-Content -path $pathlog "#   Servizi fermi: $numeroserviziS"
