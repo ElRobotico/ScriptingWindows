dc532432c77797963f9d8a74cd7bc8568f6597bb
Ruso <61048779+Rusothereal@users.noreply.github.com>
2023-03-31 11:06:14 +0200

    Create FunzioneLOG.ps1

---
+++ Lezione5/_Ruso/FunzioneLOG.ps1
@@ -0,0 +1,24 @@
+﻿function LOG {
+
+    param ($Messaggio,$Colore)
+
+    $colore = Read-Host "colore"
+
+    switch ($colore)
+    { 
+     "green" { Write-Host "INFO |" $messaggio -ForegroundColor GREEN }
+             
+     "yellow" { Write-Host "WARNING |" $messaggio -ForegroundColor YELLOW }
+         
+     "red" { Write-Host "ERROR |" $messaggio -ForegroundColor RED }
+        
+     default { Write-Host $messaggio -ForegroundColor WHITE }   
+    }
+
+}
+
+$file = "C:\log.txt"
+$data = Get-Date -Format "dd/MM/yyyy HH:mm" 
+$data = "Inizio esecuzione " + $data
+
+LOG -messaggio $data -colore $colore
