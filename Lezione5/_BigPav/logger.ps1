a5616b9279271b0fbc5365cf985a2e1b8c3e079c
b1gp <119880877+b1gp@users.noreply.github.com>
2023-03-31 10:57:53 +0200

    mandatory parameters

--- Lezione5/_BigPav/logger.ps1
+++ Lezione5/_BigPav/logger.ps1
@@ -1,23 +1,24 @@
 function log {
-    param ( $testo, $colore )
+    param ( [Parameter(Mandatory=$true)] [string] $testo, [Parameter(Mandatory=$true)][string] $colore )
     $data = Get-Date -Format "HH:mm:ss dd/MM/yyyy"
     switch ($colore) {
         "green"  { $output = "$data - Info | $testo";
-                  Write-Host $output  -ForegroundColor $colore;
-                  Out-File -FilePath "C:\temp\log.txt" -InputObject $output -Append ;
-                  break 
-                 }
-        "Yellow" { $output = "$data - Warning | $testo";
-                   Write-Host $output -ForegroundColor $colore;
-                   Out-File -FilePath "C:\temp\log.txt" -InputObject $output -Append;
+                   Write-Host $output  -ForegroundColor $colore;
                    break 
                  }
-        "Red"    { $output = "$data - ERROR | $testo";
+        "yellow" { $output = "$data - Warning | $testo";
                    Write-Host $output -ForegroundColor $colore;
-                   Out-File -FilePath "C:\temp\log.txt"-InputObject $output -Append;
+                   break 
+                 }
+        "red"    { $output = "$data - ERROR | $testo";
+                   Write-Host $output -ForegroundColor $colore;
                    break
                  }
+        Default {
+                  Write-Host "LOGGER ERROR - Unknow color selected expected: Green, Yellow or Red. Got $colore"
+                }
     }
-    
+
+    return $output;
 }
-log -testo "Inizio esecuzione" -colore "red"
+log -testo "Inizio esecuzione" -colore "red" | Out-File -FilePath "C:\temp\log.txt" -Append
